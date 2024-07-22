0ee4816eb163961ef542a3f44258140cdedf6288
AbdullahYounasDev <ab.khokhar2005@gmail.com>
2024-07-22 12:01:12 +0500

    Add user controller and bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,6 +2,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   // Get user details
@@ -16,12 +18,67 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   // Check user already exsit with same username and email
-  const exsitedUser = User.findOne({
-    $or: [{ email }, { username }],
-  });
-  if (exsitedUser) {
-    throw new ApiError(409, "User name or email already taken");
+  // User is model which is created throw mongoose and it can access data in mongodb
+
+  // const exsitedUser = User.findOne({
+  //   $or: [{ email }, { username }],
+  // });
+  // if (exsitedUser) {
+  //   throw new ApiError(409, "User name or email already taken");
+  // }
+
+  const exsitedUsername = User.findOne({ username });
+  const exsitedEmail = User.findOne({ email });
+  if (exsitedUsername) {
+    throw new ApiError(409, "User name is already taken");
   }
+  if (exsitedEmail) {
+    throw new ApiError(409, "Email is already taken");
+  }
+
+  // check the cover image and avatar come or not
+  // avatar is nessary and coverImage is not
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  // Upload on cloudinary
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+  if (!avatar) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  // Create user
+
+  const user = await User.create({
+    fullname,
+    username: username.toLowerCase(),
+    password,
+    email,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+  });
+
+  // to remove passowrd and refreshtoken
+  const userCreated = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!userCreated) {
+    throw new ApiError(
+      500,
+      "Something went wrong to register the user on server"
+    );
+  }
+
+  // Return response
+  return res
+    .status(201)
+    .json(new ApiResponse(200, "User Registerd Successfully", userCreated));
 });
 
 export { registerUser };
